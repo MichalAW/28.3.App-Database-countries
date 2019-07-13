4d1b9ece6014745f346cc77830a1fe3b87419a39
MichalAW <michal.wantaeuco@gmail.com>
2019-07-13 09:45:26 +0200

    bug

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { Provider } from 'react-redux';
 import store from './store';
 import routes from './routes';
 
-dispatch({
+render(
     <Provider store={store}>
         <Router history={hashHistory} routes={routes}/>
     </Provider>,
     document.getElementById('root')
-});
+);
