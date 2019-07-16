d4b8440f4e05e2f75bedbaba25bee640af0363d8
MichalAW <michal.wantaeuco@gmail.com>
2019-07-16 19:32:48 +0200

    dispatch

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
+import DevTools from './DevTools';
 import store from './store';
 import routes from './routes';
+import { getCountries } from './actions/actions-countries';
 
 render(
     <Provider store={store}>
-        <Router history={hashHistory} routes={routes}/>
+        <Router history={hashHistory} routes={routes}>
+            <DevTools/>
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
+
+store.dispatch(getCountries());
